22f6e4a1c35058b2a569d6f9999fbaa71b76bc14
flowerFawn <georgejparker08@Gmail.com>
2026-02-27 17:27:27 +0000

    jumping works

--- new-game-project/frog/frog.gd
+++ new-game-project/frog/frog.gd
@@ -3,8 +3,9 @@ extends CharacterBody2D
 @export var node_trajectory:Line2D
 @export var node_sprite:Sprite2D
 @export var node_ray:RayCast2D
+@export var node_collision:CollisionShape2D
 @export var a:float = -9.8
-@export var speed:float = 10
+@export var speed:float = 1
 
 var state:StringName = &"Sit" #Sit, Jump
 var u:Vector2 = Vector2.ZERO
@@ -26,12 +27,24 @@ func _input(event: InputEvent) -> void:
 			
 func jump():
 	var flip_tween:Tween = create_tween()
+	var flip_count:int
+	var t:float
 	u = get_local_mouse_position().normalized() * min(get_local_mouse_position().length() * 0.2, 50)
 	velocity = u
+	u.y = -u.y
 	node_trajectory.visible = false
 	state = &"Jump"
-	var peak:Vector2 = get_peak(velocity)
-	flip_tween.tween_property(node_sprite, "rotation", TAU, get_t(peak, find_end(peak)))
+	var peak:Vector2 = get_peak(u)
+	t = get_t(u, find_end(peak))
+	flip_count = floori(t)
+	flip_tween.tween_property(node_sprite, "rotation", TAU * flip_count, t)
+	await flip_tween.finished
+	node_sprite.rotation = 0
+	state = &"Sit"
+	node_trajectory.visible = true
+	node_collision.disabled = false
+	await get_tree().physics_frame
+	node_collision.disabled = true
 	
 func get_peak(u:Vector2) -> Vector2:
 	var s:Vector2
@@ -42,10 +55,10 @@ func get_peak(u:Vector2) -> Vector2:
 	s.x = t * u.x
 	return s
 	
-func find_end(peak:Vector2, resolution:float = 2) -> Vector2:
+func find_end(peak:Vector2, resolution:float = 1) -> Vector2:
 	#print(peak)
 	var found:bool = false
-	var distance:float = 0
+	var distance:float = 5
 	while not found and distance <= 1000:
 		node_ray.position = Vector2(distance, get_from_jump_quadratic(peak, distance))
 		node_ray.force_raycast_update()
@@ -53,12 +66,15 @@ func find_end(peak:Vector2, resolution:float = 2) -> Vector2:
 			found = true
 		else:
 			distance += resolution
-	print(node_ray.get_collision_point())
-	return node_ray.get_collision_point()
+	if found:
+		return to_local(node_ray.get_collision_point())
+	else:
+		return node_ray.position
 		
 	
-func get_t(peak:Vector2, end:Vector2) -> float:
-	return solve_quadratic((0.5 * -a), -u.y, end.y)
+func get_t(u:Vector2, end:Vector2) -> float:
+	print(end.x / u.x)
+	return (end.x / u.x) / speed
 	
 	
 func show_trajectory():
@@ -72,14 +88,15 @@ func show_trajectory():
 	for p in range(11):
 		x = ((find_end(s).x) / 10) * p
 		node_trajectory.add_point(Vector2(x, get_from_jump_quadratic(s, x)))
+	#print(find_end(s))
 	#print("s:%s, u:%s, t:%s" % [s, u, t])
 	
 
 
 func solve_quadratic(aq:float, b:float, c:float) -> float: #returns positive/higher root
 	var disc:float = max((b*b) - (4 * aq * c), 0)
-	if disc < 0:
-		print("a:%s, b:%s, c:%s" % [aq, b, c])
+	#if disc < 0:
+		#print("a:%s, b:%s, c:%s" % [aq, b, c])
 	return (-b + sqrt(disc)) / (2.0 * aq)
 	
 func get_from_jump_quadratic(peak:Vector2, x:float) -> float:
